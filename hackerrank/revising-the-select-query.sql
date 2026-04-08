ee14173ed343032a6954131ae8dc87c17c2e8e27
Gourav Gore <gaurav.gore19@gmail.com>
2026-04-08 14:24:36 +0530

    Add: revising-the-select-query

---
+++ hackerrank/revising-the-select-query.sql
@@ -0,0 +1,6 @@
+-- Problem : revising-the-select-query
+-- Platform: hackerrank
+-- URL     : https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+-- Solved  : 2026-04-08T08:54:37.969Z
+-- 
+select * from city where countrycode='USA' and population>100000;
